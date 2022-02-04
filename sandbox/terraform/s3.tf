7ac21ae7dab623d97d3db29e05650f3302382b77
Brian Schreier <bschreier@bandwidth.com>
2022-02-04 11:14:17 -0500

    PLAT-1792 Test pipeline kickoff

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,8 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-demo-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "another_sandbox_bucket" {
+  bucket = "aft-demo-2${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
